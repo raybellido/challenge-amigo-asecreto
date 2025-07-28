38bddf12497b2873c177907536624db45722d09e
ray <raaygb@gmail.com>
2025-07-28 16:38:36 -0500

    terminado

--- app.js
+++ app.js
@@ -1,2 +1,32 @@
-// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
-abc
+const listaAmigos = [] // esto es para ingresar y guardar los nombres
+
+function agregarAmigo(){
+         const imputAmigo = document.getElementById("amigo");
+         const ullistadeamigos = document.getElementById("listaAmigos");
+         ullistadeamigos.innerHTML  += `<li>${imputAmigo.value}</li>`;
+
+        if(imputAmigo.value === ''){
+            alert('por favor inserte un nombre')
+        }
+        else { listaAmigos.push(imputAmigo.value) }
+
+        imputAmigo.value = ''
+}
+
+   function sortearAmigo(){
+        const sorteo = Math.floor(Math.random() * listaAmigos.length)
+       
+        const amigosecreto =  listaAmigos[sorteo]
+
+        const resultado = document.getElementById('resultado')
+        resultado.innerHTML = `<li>tu amigo secreto es ${amigosecreto}</li>`
+
+    }
+
+
+
+
+
+    
+
+
